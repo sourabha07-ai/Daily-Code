8a085cb19460a03733865952238a0139dd563bb5
Sourabha Jena <sourabhajena2003@gmail.com>
2026-07-20 08:32:38 +0530

    Modify

--- DPP/Dpp_2/Questions11-20/Q12_checkDivisible3and5.cpp
+++ DPP/Dpp_2/Questions11-20/Q12_checkDivisible3and5.cpp
@@ -1,6 +1,6 @@
-// Use a comprehensive header to avoid missing include issues in some environments
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
 
 // Take a positive integer input from the user and check if it is divisible by both 3 and
 // 5. Do not use the logical AND (&&) operator inside your conditional test statement. (Hint:
@@ -9,14 +9,14 @@ using namespace std;
 // Sample — Input: 30 ⇒ Output: Divisible by both 3 and 5
 
 int main(){
-    int integer ;
-    cout <<"Input: ";
-    cin >> integer;
+    int integer;
+    std::cout << "Input: ";
+    std::cin >> integer;
 
     if (integer % 15 == 0) {
-        cout << "Divisible by both 3 and 5";
+        std::cout << "Divisible by both 3 and 5";
     } else {
-        cout << "Not divisible by both 3 and 5";
+        std::cout << "Not divisible by both 3 and 5";
     }
      
 
