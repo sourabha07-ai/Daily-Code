2ba112786a4906055f8b220a2f714e34ac615d36
Sourabha Jena <sourabhajena2003@gmail.com>
2026-07-29 23:40:32 +0530

    array

--- Day_15_Array/Pw_Array/PrintAllElements.cpp
+++ Day_15_Array/Pw_Array/PrintAllElements.cpp
@@ -1,12 +1,16 @@
  #include <iostream>
  #include <string>
 using namespace std;
+#define r "\033[31m"
+#define y "\033[33m"
+#define reset "\033[0m"
+
 
 int main() {
     int nums[] = {10, 20, 30, 40, 50};
     // cout <<size(nums) <<endl;
 
     for (size_t i = 0; i < size(nums); i++) {
-        cout << nums[i] << " ";
+        cout <<r << nums[i]  <<reset << " ";
     }
 }
