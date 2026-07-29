51cf857fd93bfcb1029cd07c51487eba43fe5f63
Sourabha Jena <sourabhajena2003@gmail.com>
2026-07-29 21:47:41 +0530

    array

--- Day_15_Array/Pw_Array/PrintAllElements.cpp
+++ Day_15_Array/Pw_Array/PrintAllElements.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
     int nums[] = {10, 20, 30, 40, 50};
+    // cout <<size(nums) <<endl;
 
     for (size_t i = 0; i < size(nums); i++) {
         cout << nums[i] << " ";
