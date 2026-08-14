884f18d4cc76bdd7e3b461f71b48d83cc6649736
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-15 05:20:34 +0530

    wave array

--- LeetCode/WaveArray.cpp
+++ LeetCode/WaveArray.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
+void waveArray(vector<int>& arr){
+     for(int i = 0; i < arr.size() - 1; i += 2){
+          swap(arr[i],arr[i+1]);
+     }
+
+     cout <<endl;
+     
+     cout <<"Wave Array: ";
+     for(int a:arr){
+          cout <<a <<" ";
+     }
+}
+
 int main(){
-     cout <<"Hell";
+     vector<int> arr ={1,2,3,4,5,6};
+     cout <<"Original Array: ";
+     for(int n:arr){
+          cout <<n <<" ";
+     }
+     waveArray(arr);
+
+     
 }
