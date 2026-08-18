ddce7dc46c1ffd3f3885d9c47f61743b950f2d12
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-18 21:39:50 +0530

    dpp-06

--- DPP/Dpp_6/Questions_08_to_18/Question_09.cpp
+++ DPP/Dpp_6/Questions_08_to_18/Question_09.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include<vector>
 using namespace std; 
 
 int main(){
-  
-    cout <<endl;
+   vector<int> v;
+   v.push_back(5);
+   v.push_back(10);
+   v.push_back(15);
+   v.pop_back();
+   cout <<v.size()<<" " <<v.capacity();
     
     return 0;
 }
