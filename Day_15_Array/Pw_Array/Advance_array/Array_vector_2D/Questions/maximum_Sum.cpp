a75b2ac08aa9363d935fc512f11229e42d7553f4
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-20 04:41:40 +0530

    maximum Sum array

--- Day_15_Array/Pw_Array/Advance_array/Array_vector_2D/Questions/maximum_Sum.cpp
+++ Day_15_Array/Pw_Array/Advance_array/Array_vector_2D/Questions/maximum_Sum.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
+#define g "\033[32m"
+#define reset "\033[0m"
+
 
 int main(){
-    int arr[][4] = {{1,2,1,0},{2,3,1,1},{2,1,3,1}};
-    int sum = 0;
+    int arr[][3] = {{9,4,3},{2,3,7},{8,7,8}};
+    int maxRow = -1; int maxSum = INT_MIN;
     for(int i = 0; i < 3;i++){
-         for(int j = 0; j < 4;j++){
-            if(i >= 0){
-                
-            }
+        int sum = 0;
+         for(int j = 0; j < 3;j++){
+            cout <<arr[i][j] <<" ";
+             sum += arr[i][j];
          }
+         cout <<endl;
+         if(sum > maxSum){
+            maxSum = sum;
+            maxRow = i;
+         }
+         
     }
+    cout <<"Max Row no. " <<g <<maxRow <<reset <<endl;
+    cout <<"Maximum Sum: " <<g <<maxSum <<reset <<endl;
 
 }
