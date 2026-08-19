17e2471a06138097e10597b3bb469d57ffcc18b5
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-20 04:34:06 +0530

    matrix Sum

---
+++ Day_15_Array/Pw_Array/Advance_array/Array_vector_2D/Questions/matrix_Sum_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[][4] = {{1,2,1,0},{2,3,1,1},{2,1,3,1}};
+    int sum = 0;
+    for(int i = 0; i < 3;i++){
+         for(int j = 0; j < 4;j++){
+            sum = sum + arr[i][j];
+         }
+    }
+    cout <<sum;
+    
+}
---
+++ Day_15_Array/Pw_Array/Advance_array/Array_vector_2D/Questions/maximum_Sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[][4] = {{1,2,1,0},{2,3,1,1},{2,1,3,1}};
+    int sum = 0;
+    for(int i = 0; i < 3;i++){
+         for(int j = 0; j < 4;j++){
+            if(i >= 0){
+                
+            }
+         }
+    }
+
+}
