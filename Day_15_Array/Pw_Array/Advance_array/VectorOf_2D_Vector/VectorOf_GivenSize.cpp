371c00cecef31680ea0fa68f3346f1de77770715
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-22 15:39:17 +0530

    vector

--- Day_15_Array/Pw_Array/Advance_array/VectorOf_2D_Vector/VectorOf_GivenSize.cpp
+++ Day_15_Array/Pw_Array/Advance_array/VectorOf_2D_Vector/VectorOf_GivenSize.cpp
@@ -13,9 +13,6 @@ int main(){
     vector<vector<int>>vec(m,vector<int>(n,-1));
 
     vec.push_back(vector<int>(4,0));
-     
-
-
     //out put
     for(int i = 0; i <vec.size();i++){
         for(int j = 0; j < vec[i].size();j++){
@@ -23,10 +20,6 @@ int main(){
         }
         cout <<endl;
     }
-
-
-
-
 
     return 0;
 }
