fce2bc378612eef33379cc235fd7cbbf705673dc
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-23 20:19:37 +0530

    Rotate array

--- DPP/Dpp_6/Question_19_to_30/Question_20.cpp
+++ DPP/Dpp_6/Question_19_to_30/Question_20.cpp
@@ -8,7 +8,20 @@ Explanation: Right by 2 gives {4, 5, 1, 2, 3}. Then left by 1 gives
 {5, 1, 2, 3, 4}.
 */
 #include <iostream>
+#include<vector>
 using namespace std;
+
+#define g "\033[32m"
+#define yellow "\033[33m"
+#define reset "\033[0m"
+
+void reverseArray(vector<int>&arr,int start,int end){
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
 
 
 int main(){
@@ -16,7 +29,11 @@ int main(){
     cout <<"Enter size of array: ";
     cin >> n;
 
-     int arr[n];
+     if(n==0){
+        return 0;
+    }
+
+    vector<int> arr(n);
     for(int i = 0; i < n;i++){
         cout <<"Enter " <<i <<" index: ";
         cin >> arr[i];
@@ -25,18 +42,31 @@ int main(){
     for(int a:arr){
         cout <<a <<" ";
     }
+    
+    cout <<endl;
+
+    long long x,y;
+    cout <<"first right-rotate the array by X: ";
+    cin >>x;
+    cout <<"left-rotate the resulting array by Y: ";
+    cin >> y;
+
+    long long k = (x - y) % n;
+    if( k < 0){
+       k = k + n;
+    }
+     
+    reverseArray(arr,0,n-1);
+    reverseArray(arr,0,(int)k-1);
+    reverseArray(arr,(int)k,n-1);
 
     cout <<endl;
-    cout <<"Rotate array: ";
-    for(int i = 0; i < n;i++){
-        swap(arr[i],arr[n-1]);
-        cout << arr[i] <<" ";
+    cout <<yellow <<"rotate array: "<<reset;
+    for(int a: arr){
+        cout <<a <<" ";
     }
-
-
-
-
-
+    cout <<endl;
+    
     return 0;
 
 }
