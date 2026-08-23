242edc91b21c8f8723c70c5b17ffc9db086f02ad
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-24 00:44:22 +0530

    Question 23

---
+++ DPP/Dpp_6/Question_19_to_30/Question_23.cpp
@@ -0,0 +1,48 @@
+/*
+First Repeated Element. Given an array of size N , find the first element from the
+left that appears more than once in the array. If no element repeats, print -1.
+Constraints: 1 ≤N ≤105, 0 ≤arr[i] ≤105.
+Sample Input: N = 7, arr = {10, 5, 3, 4, 3, 5, 6} ⇒ Output: 5
+Explanation: Both 5 and 3 are repeated, but traversing from the left, 5 is the first element we
+encounter that will be repeated later.
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+#define g "\033[32m"
+#define y "\033[33m"
+#define reset "\033[0m"
+
+
+void checkRepeatNumber(vector<int>& arr,int n){
+    for(int i = 0; i <n; i++){
+        for(int j = i+1; j < n; j++){
+            if(arr[i] == arr[j]){
+               cout <<g <<"First Repeat Number: " <<reset << arr[i];
+                return;
+            }
+        }
+    }
+}
+
+
+int main(){
+   int n ;
+   cout <<"Enter size of array: ";
+   cin >> n;
+   
+   vector<int> vec(n);
+   for(int i = 0; i < n;i++){
+       cout <<"Enter " <<i <<" index: ";
+       cin >> vec[i];
+   }
+   cout <<endl;
+   cout <<"Your array is : ";
+   for(int a:vec) cout <<a <<" ";
+
+   cout <<endl;
+
+    checkRepeatNumber(vec,n);
+   
+  return 0;
+}
