435b3c5edc166a2fedfca4dc5f52ee516633c8ac
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-27 09:48:00 +0530

    Print_matrix.cpp

--- Day_15_Array/Pw_Array/Advance_array/2D_Array/Print_matrix.cpp
+++ Day_15_Array/Pw_Array/Advance_array/2D_Array/Print_matrix.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
-#include<vector>
+using namespace std;
+
+int main(){
+    int arr[3][4] = 
+    {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+
+    cout <<"Matrix: "<<endl;
+    for(int i = 0; i < size(arr);i++){
+        for(int j = 0; j < size(arr[0]);j++){
+            cout <<arr[i][j] <<" ";
+        }
+        cout <<endl;
+    }
+
+    return 0;
+}
