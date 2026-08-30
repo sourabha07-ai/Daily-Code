0c21cc89bed05b3ebd1978c8d1dc19558da92356
Sourabha Jena <sourabhajena2003@gmail.com>
2026-08-30 08:02:31 +0530

    9_Count_Negatives.cpp

--- DPP/Dpp_7/Question_08_to_14/9_Count_Negatives.cpp
+++ DPP/Dpp_7/Question_08_to_14/9_Count_Negatives.cpp
@@ -1,41 +1,66 @@
-/*
-Q9. Count Negatives. Given an M ×N matrix which is sorted in non-increasing order both
-row-wise and column-wise, count the number of negative numbers. Find an optimal approach
-that runs in O(M + N ) time complexity.
-Constraints: 1 ≤M, N ≤104, −105≤arr[i][j] ≤105.
-Sample Input: {{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}}
-Sample Output: 8
-Q10. Toeplitz Matrix. A matrix is a Toeplitz matrix if every diagonal from top-left to
-bottom-right has the same elements. Write a function to check if a given M ×N matrix is a
-Toeplitz Matrix. Return true or false.
-Constraints: 1 ≤M, N ≤103.
-Sample Input: {{1, 2, 3, 4}, {5, 1, 2, 3}, {9, 5, 1, 2}}
-Sample Output: true
-Q11. Rotate Matrix by 180 Degrees. Given an N ×N square matrix, rotate it by 180
-degrees in-place. You must not use any extra 2D array for the final result.
-Constraints: 1 ≤N ≤103.*/
 
-#include<iostream>
-#include<vector>
+// Q9. Count Negatives. Given an M ×N matrix which is sorted in non-increasing order both
+// row-wise and column-wise, count the number of negative numbers. Find an optimal approach
+// that runs in O(M + N ) time complexity.
+// Constraints: 1 ≤M, N ≤104, −105≤arr[i][j] ≤105.
+// Sample Input: {{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}}
+// Sample Output: 8
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-bool isToeplitz(const vector<vector<int>>& mat) {
-    int rows = mat.size();
-    if (rows == 0) return true;
+int countNegatives(vector<vector<int>> &arr){
+    int m = arr.size();
+    int n = arr[0].size();
 
-    int cols = mat[0].size();
-    for (int i = 1; i < rows; ++i) {
-        for (int j = 1; j < cols; ++j) {
-            if (mat[i][j] != mat[i - 1][j - 1]) {
-                return false;
-            }
+    int row = 0;
+    int col = n - 1;
+    int count = 0;
+
+    while (row < m && col >= 0){
+        if (arr[row][col] < 0){
+            count += m - row;
+            col--;
+        }
+        else{
+            row++;
         }
     }
-    return true;
+
+    return count;
 }
 
-int main() {
-    vector<vector<int>> arr = {{1, 2, 3, 4}, {5, 1, 2, 3}, {9, 5, 1, 2}};
-    cout << boolalpha << isToeplitz(arr) << endl;
+int main(){
+    int m, n;
+    cout << "Enter rows: ";
+    cin >> m;
+    cout << "Enter cloumn: ";
+    cin >> n;
+
+    vector<vector<int>> arr(m, vector<int>(n));
+
+    cout << "Enter matrix elements:\n";
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            cout <<"Enter-" <<i <<j <<" index: ";
+            cin >> arr[i][j];
+        };
+    };
+    cout <<"matrix is: "<<endl;
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            cout <<arr[i][j] <<" ";
+        }
+        cout <<endl;
+    };
+
+
+
+    cout << "Number of negative numbers: "
+         << countNegatives(arr) << endl;
+
     return 0;
 }
